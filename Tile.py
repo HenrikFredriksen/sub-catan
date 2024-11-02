ca001bf4187270e52febf416136e06bd1f377e52
Henrik Fredriksen <henrikfred03@gmail.com>
2024-11-02 19:36:21 +0100

    1: add tile and coordinate classes

---
+++ Tile.py
@@ -0,0 +1,8 @@
+
+class Tile:
+    def __init__(self, resource, number, position):
+        self.resource = resource
+        self.number = number
+        self.position = position # HexCoordinate
+        
+    
