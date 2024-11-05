0a530da85722117c435a24ae6cae0f07b8f38b18
Henrik Fredriksen <henrikfred03@gmail.com>
2024-11-05 01:42:11 +0100

    2: modify click to check for edges, set new rules for placing roads and houses

--- GameManager.py
+++ GameManager.py
@@ -1,4 +1,6 @@
 from House import House
+from Road import Road
+import numpy as np
 
 class GameManager:
     def __init__(self, game_board, game_rules):
@@ -9,13 +11,42 @@ class GameManager:
         
     def handle_click(self, mouse_pos):
         print(f"Mouse clicked at: {mouse_pos}")
-        nearest_vertex = self.game_board.find_nearest_vertex(mouse_pos)
-        if nearest_vertex:
-            print(f"Nearest vertex: {nearest_vertex.position}")
-            if self.game_rules.is_valid_house_placement(nearest_vertex):
-                house = House(vertex=nearest_vertex, player=self.current_player)
-                nearest_vertex.house = house
-                print(f"Placed house at {nearest_vertex.position}")
+        proximity_radius = self.game_board.hex_size / 4
+        
+        nearest_vertex = self.game_board.find_nearest_vertex(mouse_pos, proximity_radius)
+        nearest_edge = self.game_board.find_nearest_edge(mouse_pos, proximity_radius)
+        
+        if nearest_vertex and nearest_edge:
+            vertex_distance = np.hypot(mouse_pos[0] - nearest_vertex.position[0], mouse_pos[1] - nearest_vertex.position[1])
+            edge_distance = np.hypot(mouse_pos[0] - (nearest_edge.vertex1.position[0] + nearest_edge.vertex2.position[0]) // 2, 
+                                     mouse_pos[1] - (nearest_edge.vertex1.position[1] + nearest_edge.vertex2.position[1]) // 2)
+            if vertex_distance < edge_distance:
+                self.place_house(nearest_vertex)
             else:
-                print("Invalid placement")
-                
+                self.place_road(nearest_edge)
+        elif nearest_vertex:
+            self.place_house(nearest_vertex)
+        elif nearest_edge:
+            self.place_road(nearest_edge)
+        else:
+            print("No vertex or edge found")
+            
+
+    def place_house(self, vertex):
+        print(f"Nearest vertex: {vertex.position}")
+        if self.game_rules.is_valid_house_placement(vertex):
+            house = House(vertex=vertex, player=self.current_player)
+            vertex.house = house
+            print(f"Placed house at {vertex.position}")
+        else:
+            print("Invalid House placement")
+            
+    def place_road(self, edge):
+        print(f"Nearest edge: {edge.vertex1.position} - {edge.vertex2.position}")
+        if self.game_rules.is_valid_road_placement(edge, self.current_player):
+            road = Road(vertex1=edge.vertex1, vertex2=edge.vertex2, player=self.current_player)
+            edge.road = road
+            print(f"Placed road at {edge.vertex1.position} - {edge.vertex2.position}")
+        else:
+            print("Invalid Road placement")
+        
--- GameRules.py
+++ GameRules.py
@@ -6,4 +6,28 @@ class GameRules:
     def is_valid_house_placement(self, vertex):
         if vertex.house is not None:
             return False
+        
+        # check if there is a house on any of the neighbors
+        for neighbor in vertex.neighbors:
+            if neighbor.house is not None:
+                return False
         return True
+    
+    def is_valid_road_placement(self, edge, player):
+        if edge.road is not None:
+            return False
+        
+        if edge.vertex1.house and edge.vertex1.house.player == player:
+            return True
+        if edge.vertex2.house and edge.vertex2.house.player == player:
+            return True
+        
+        for neighbor in edge.vertex1.neighbors:
+            if neighbor != edge.vertex2 and self.game_board.edges.get(tuple(sorted([edge.vertex1.position, neighbor.position]))):
+                if self.game_board.edges[tuple(sorted([edge.vertex1.position, neighbor.position]))].road and self.game_board.edges[tuple(sorted([edge.vertex1.position, neighbor.position]))].road.player == player:
+                    return True
+        for neighbor in edge.vertex2.neighbors:
+            if neighbor != edge.vertex1 and self.game_board.edges.get(tuple(sorted([edge.vertex2.position, neighbor.position]))):
+                if self.game_board.edges[tuple(sorted([edge.vertex2.position, neighbor.position]))].road and self.game_board.edges[tuple(sorted([edge.vertex2.position, neighbor.position]))].road.player == player:
+                    return True
+        return False
