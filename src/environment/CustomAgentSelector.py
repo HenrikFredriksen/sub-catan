cab00e6a64a3bc7b9f8923275a6c3ad28b959384
Henrik Fredriksen <henrikfred03@gmail.com>
2024-11-10 01:52:18 +0100

    4: add custom agent selction class for settlephase

---
+++ src/environment/CustomAgentSelector.py
@@ -0,0 +1,16 @@
+
+class CustomAgentSelector:
+    def __init__(self, agent_order):
+        self.agent_order = agent_order
+        self.current_agent_index = 0
+        
+    def reset(self):
+        self.current_agent_index = 0
+        
+    def next(self):
+        if self.current_agent_index < len(self.agent_order):
+            agent_selection = self.agent_order[self.current_agent_index]
+            self.current_agent_index += 1
+            return agent_selection
+        else:
+            return None
