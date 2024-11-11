59678fac68c5876bccbbc353eafc12fc3a83a4bd
Henrik Fredriksen <henrikfred03@gmail.com>
2024-11-11 05:16:16 +0100

    4: fix rllib api policy expectations

--- src/train.py
+++ src/train.py
@@ -1,39 +1,62 @@
 import ray
 from ray import tune
-from ray.rllib.agents.ppo import PPOTrainer
-from env.CatanEnv import CatanEnv
+from ray.rllib.algorithms.ppo import PPOConfig
+from ray.rllib.algorithms.ppo.ppo_tf_policy import PPOTF2Policy
+from ray.rllib.utils.framework import try_import_tf
+from environment.CatanEnv import CatanEnv
+from ray.rllib.env import PettingZooEnv
+
+tf1, tf, tfv = try_import_tf()
+
+class CustomPPOTF2Policy(PPOTF2Policy):
+    def optimizer(self):
+        return tf.keras.optimizers.Adam(learning_rate=float(self.config["lr"]))
 
 def env_creator(env_config):
-    return CatanEnv()
+    env = CatanEnv()
+    return PettingZooEnv(env)
 
 ray.init()
 
 tune.register_env("catan", env_creator)
 
-config = {
-    "env": "catan",
-    "framework": "tf",
-    "num_gpus": 1,
-    "num_workers": 0,
-    "multiagent": {
-        "policies": {
-            "player_1": (None, CatanEnv.observation_spaces['player1'], CatanEnv.action_spaces['player1'], {}),
-            "player_2": (None, CatanEnv.observation_spaces['player2'], CatanEnv.action_spaces['player2'], {}),
-            "player_3": (None, CatanEnv.observation_spaces['player3'], CatanEnv.action_spaces['player3'], {}),
-            "player_4": (None, CatanEnv.observation_spaces['player4'], CatanEnv.action_spaces['player4'], {}),
+env_instance = CatanEnv()
+env = PettingZooEnv(env_instance)
+
+ppo_config = (
+    PPOConfig()
+    .environment("catan")
+    .framework("tf2")
+    .env_runners(num_env_runners=0)
+    .multi_agent(
+        policies={
+            "shared_policy": (
+                CustomPPOTF2Policy, 
+                env.observation_space, 
+                env.action_space, 
+                {}
+            ),
         },
-        "policy_mapping_fn": lambda agent_id: "agent_id",
-    },
-}
+        policy_mapping_fn=lambda agent_id, episode, **kwargs: "shared_policy",
+    )
+    .training(
+        lr=0.0001,
+    )
+    .resources(
+        num_gpus=1,
+    )
+)
 
-trainer = PPOTrainer(env="catan", config=config)
+algo = ppo_config.build()
 
-for i in range(1000):
-    result = trainer.train()
+
+
+for i in range(100):
+    result = algo.train()
     print(f"iteration: {i}, reward: {result['episode_reward_mean']}")
     
-    if i % 50 == 0:
-        checkpoint = trainer.save()
+    if i % 10 == 0:
+        checkpoint = algo.save()
         print(f"checkpoint saved at {checkpoint}")
         
 ray.shutdown()
