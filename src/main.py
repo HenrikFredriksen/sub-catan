84c6e7472f656f9947cc58329f09f4b005d5c79e
Henrik Fredriksen <henrikfred03@gmail.com>
2025-04-10 19:45:59 +0200

    6: Add argument parser to entrypoint in main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import argparse
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from models.Evaluator import eval_trained_agents
@@ -7,38 +9,61 @@ from models.Trainer import train, train_settlement_phase
 from gameloop.GameLoop import GameLoop
 from models.test_catan_env_random_action import test_environment
 
+def argparser():
+    parser = argparse.ArgumentParser(description="Train Catan agents")
+    parser.add_argument(
+        "--train_settle_phase",
+        type=bool,
+        default=False,
+        help="Train agents in the settlement phase of the game",
+    )
+    parser.add_argument(
+        "--train",
+        type=bool,
+        default=False,
+        help="Train agents in the normal phase of the game",
+    )
+    parser.add_argument(
+        "--eval",
+        type=int,
+        default=0,
+        help="Evaluate trained agents, how many times to evaluate",
+    )
+    parser.add_argument(
+        "--test_env",
+        type=bool,
+        default=False,
+        help="Test the environment by taking random actions for each agent",
+    )
+    parser.add_argument(
+        "--interactive",
+        type=bool,
+        default=False,
+        help="Run the interactive game loop",
+    )
+    return parser.parse_args()
+
+def main():
+    args = argparser()
+    
+    if args.train_settle_phase:
+        train_settlement_phase()
+    elif args.train:
+        train(n_episodes=2500)
+    elif args.eval != 0:
+        for _ in range (args.eval):
+            eval_trained_agents(render_mode='human', gamestate='normal_phase')
+    elif args.test_env:
+        test_environment()
+    elif args.interactive:
+        GameLoop().main()
+    else:
+        print("No valid argument provided. Use --help for more information.")
+
+
 if __name__ == "__main__":
     '''
     this is the main entry point for the project.
     uncomment the function you want to run.
-    
-    main() - this is the main game loop, not used for model training, just for playing the game.
-    
-    test_environment() - Test the environment by taking random actions for each agent
-    
-    train() - Train models, takes optional argument: 
-        - gamestate='normal_phase' or 'settle_phase'
-        
-    train_settlement_phase() - Train models only in settle phase of the game
-    
-    eval_trained_agents() - Evaluate the trained agents, takes optional arguments: 
-        - render_mode='rgb_array' or 'human'
-        - gamestate='normal_phase' or 'settle_phase'
     '''
-    
-    #GameLoop().main()
-    
-    #train_settlement_phase()
-    
-    #test_environment()
-    
-    # Change gamestate 'settle_phase' to run whole game training
-    # Change gamestate 'normal_phase' to run normal phase training with loaded board
-    #train(n_episodes=5000) # Train models in normal phase of the game with a loaded board state
-    
-
-    # render_mode = 'rgb_array' or 'human'
-    # gamestate = 'normal_phase' or 'settle_phase'
-    num_evals = 3
-    for _ in range(num_evals):
-        eval_trained_agents(render_mode='human', gamestate='normal_phase') # Evaluate the trained agents
+    main()
